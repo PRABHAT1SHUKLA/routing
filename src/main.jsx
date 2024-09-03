e28181f0a19c0202fdd08d689123cc44465747cd
prabhat <prabhatshukla0849@gmail.com>
2024-09-03 18:18:24 +0530

    added error handling route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
 import './index.css'
+import Root from './routes/root';
+import ErrorPage from './error-page';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement:<ErrorPage/>
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
