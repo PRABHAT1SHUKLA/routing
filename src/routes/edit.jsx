569a3e27da5a75d81f1758d0b85604558d10642f
prabhat <prabhatshukla0849@gmail.com>
2024-09-04 12:30:10 +0530

    added search functionality

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,4 +1,4 @@
-import { Form, useLoaderData , redirect} from "react-router-dom";
+import { Form, useLoaderData , redirect, useNavigate} from "react-router-dom";
 import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
@@ -12,6 +12,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const { contact } = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <Form method="post" id="contact-form">
@@ -61,7 +62,10 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button"
+        onClick={() => {
+            navigate(-1);
+        }}>Cancel</button>
       </p>
     </Form>
   );
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,11 @@
 import { Link, Outlet , useLoaderData , Form, redirect, NavLink, useNavigation, } from "react-router-dom";
 import { getContacts , createContact} from "../contacts";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({request}) {
+   const url = new URL(request.url);
+   const q = url.searchParams.get("q"); 
+   
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
@@ -25,7 +28,7 @@ export default function Root() {
 
 
         <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
@@ -42,7 +45,7 @@ export default function Root() {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
