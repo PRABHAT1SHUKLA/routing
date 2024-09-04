d39d3c0f8656242ae94bc2fedf998c85f14fcbe4
prabhat <prabhatshukla0849@gmail.com>
2024-09-04 12:34:19 +0530

    searchbar refresh bug fixed

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,7 +6,7 @@ export async function loader({request}) {
    const q = url.searchParams.get("q"); 
    
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts , q};
 }
 
 export async function action(){
@@ -15,7 +15,7 @@ export async function action(){
 }
 
 export default function Root() {
-    const {contacts } = useLoaderData();
+    const {contacts ,q} = useLoaderData();
     const navigation = useNavigation();
 
     return (
@@ -35,6 +35,7 @@ export default function Root() {
                 placeholder="Search"
                 type="search"
                 name="q"
+                defaultValue={q}
               />
               <div
                 id="search-spinner"
