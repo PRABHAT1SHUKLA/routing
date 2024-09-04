8b6dcc48b62f79c95dfb7106b8185aeb09a7b666
prabhat <prabhatshukla0849@gmail.com>
2024-09-04 13:23:58 +0530

    added pathless route to catch error thrown in child routes , preserving the root route's ui

--- src/main.jsx
+++ src/main.jsx
@@ -26,25 +26,32 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      { index: true , element: <Index/>},
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path:"contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div> Oops! there was an error.</div>
-      }
-      
+     {
+      errorElement:<ErrorPage/>,
+      children:[
+        { index: true , element: <Index/>},
+        {
+          path: "contacts/:contactId",
+          element: <Contact />,
+          loader: contactLoader,
+          action: contactAction,
+        },
+        {
+          path: "contacts/:contactId/edit",
+          element: <EditContact />,
+          loader: contactLoader,
+          action: editAction,
+        },
+        {
+          path:"contacts/:contactId/destroy",
+          action: destroyAction,
+          errorElement: <div> Oops! there was an error.</div>
+        },
+        
+      ],
+     },   
+
+     
     ],
   },
 ]);
