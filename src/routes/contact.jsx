32be34d4030a7eb29663bd94bf1dd16349359cbb
prabhat <prabhatshukla0849@gmail.com>
2024-09-04 13:06:03 +0530

    configured favourites functionality using fetcher so that no new navigation takes place while we favourite our contact and also created a action in our contact route for updation in our database

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,10 +1,18 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useFetcher, useLoaderData } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
   return { contact };
 }
+
+export async function action({request, params}){
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favourite: formData.get("favourite") ==="true",
+  });
+}
+
 
 export default function Contact() {
   const { contact } = useLoaderData();
@@ -72,9 +80,10 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
+  const fetcher = useFetcher();
   const favorite = contact.favorite;
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -86,6 +95,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
